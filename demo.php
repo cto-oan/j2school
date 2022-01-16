bf0e59b89ad1e6e079e38b76317cae9a6d166111
cto-oan <congtoansky14@gmail.com>
2022-01-16 15:59:20 +0700

    Update demo.php

---
+++ demo.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $a = 5;
+        if($a < 10){
+            echo "Số $a nhỏ hơn 10";
+        }else{
+            echo "Số $a lớn hơn 10";
+        }
+    ?>
+<script type="text/javascript">
+    let a = 10;
+    
+</script>
+</body>
+</html>
